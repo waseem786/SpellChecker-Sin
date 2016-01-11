f2c3e28b2537fa36f37ff15934a4b98de10b5ba4
Asanka Wasala <asanka.wasala@lero.ie>
2016-01-11 14:40:06 +0000

    corrected a typo and removed the unnecessary root route handler

--- spellchecker_API.py
+++ spellchecker_API.py
@@ -10,13 +10,11 @@ import libxml2
 import libxslt
 from spellchecker import Spellchecker_si
 
-
-@route('/')
-@get('/spellcheck/<word>')
+@route('/spellcheck/<word>')
 def spellcheck(word=word):
 
     xml_output = Spellchecker_si().spellcheck(word, format='FULLXML')
-    json_outout = xmldict.xml_to_dict(xml_output)
+    json_output = xmldict.xml_to_dict(xml_output)
     
     return xml_output
 
